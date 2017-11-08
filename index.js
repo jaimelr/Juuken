66965451fb76068afc89a3e1b5e2a256d0498442
Jaime <j.loyolarangel@gmail.com>
2017-11-07 19:25:59 -0600

    Create main GET route

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Get express dependecy
 var express = require('express');
+var path = require('path');
 var app = express(); // Instantiate it
+
+var indexEP = require('./routes/index');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Browser routes
+app.use('/', indexEP);
 
 // Server configuration
 app.set('port', 3000); // Set port number to listen to
