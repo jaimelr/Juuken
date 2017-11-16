21dd7281959b9c2d6abb520594f7afe5ea73b95f
Jaime <j.loyolarangel@gmail.com>
2017-11-16 15:31:59 -0600

    Add Angular and angular routes dependecies

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var PORT = process.env.PORT || 3000;
 var indexEP = require('./routes/index');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 
 // Browser routes
 app.use('/', indexEP);
