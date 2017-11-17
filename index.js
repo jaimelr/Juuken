4c6886612f3eea1c05e103d0a776964e69d0804c
Jaime <j.loyolarangel@gmail.com>
2017-11-16 19:51:12 -0600

    Create angular routing

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ var app = express(); // Instantiate it
 var PORT = process.env.PORT || 3000;
 
 var indexEP = require('./routes/index');
+var devexEP = require('./routes/devex');
+var devcheatEP = require('./routes/devcheat');
+var microblogEP = require('./routes/microblog');
+var opinionEP = require('./routes/opinion');
+var bioEP = require('./routes/bio');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 
 // Browser routes
 app.use('/', indexEP);
+app.use('/devex', devexEP);
+app.use('/devcheat', devcheatEP);
+app.use('/microblog', microblogEP);
+app.use('/opinion', opinionEP);
+app.use('/bio', bioEP);
 
 // Server configuration
 app.set('port', PORT); // Set port number to listen to
